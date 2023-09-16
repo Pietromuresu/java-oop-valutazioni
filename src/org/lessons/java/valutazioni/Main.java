2cf6c6de84b97364879ae62e36cec3bd8ae3b3ae
Pietromuresu <muresupietro66@gmail.com>
2023-09-16 12:49:34 +0200

    Bonus - Part 1

--- src/org/lessons/java/valutazioni/Main.java
+++ src/org/lessons/java/valutazioni/Main.java
@@ -8,19 +8,67 @@ public class Main {
 		
 		
 		Random rand = new Random();
-		Studente[] students = new Studente[20];
+		Studente[] studenti = new Studente[20];
+		
+		int lunghezzaPromossi = 0;
+		int lunghezzaBocciati = 0;
+		
 		int totPromossi = 0;
-		for(int i = 1; i < students.length + 1; i++) {
+		
+		// Popolo l'array di studenti con dati random 
+		for(int i = 1; i < studenti.length + 1; i++) {
 	
-			students[i -1] = new Studente(i, rand.nextInt(0, 101), rand.nextFloat(0, 5.1f));
-			students[i -1].showDatas();
-			if(students[i - 1].hasFailed().equals("Promosso")) {
-				totPromossi++;
+			studenti[i -1] = new Studente(i, rand.nextInt(0, 101), rand.nextFloat(0, 5.01f));
+			studenti[i -1].showDatas();
+			// Conto il totale dei promossi e la
+			if(studenti[i - 1].hasFailed().equals("Promosso")) {
+				
+				lunghezzaPromossi++;
+				totPromossi++; 
+			}else {
+				
+				lunghezzaBocciati++;
 			}
+			
 		}
 		
-		System.out.println("Totale promossi: " + totPromossi);
+		// Creo gli array per i promossi e bocciati e inizializzo gli indici
+			int indicePromossi = 0;
+			int indiceBocciati = 0;
+			Studente[] promossi = new Studente[lunghezzaPromossi]; 
+			Studente[] bocciati = new Studente[lunghezzaBocciati];
+			
+			for(int i = 0; i < studenti.length; i++) {
+				
+				if(studenti[i].hasFailed().equals("Promosso")){
+					
+					promossi[indicePromossi] = studenti[i];
+					indicePromossi++;
+				}else {
+					bocciati[indiceBocciati] = studenti[i];
+					indiceBocciati++;
+				}
+			}
+			
+			// Stampo il totale dei promossi
+			System.out.println("Totale promossi: " + totPromossi);
+			
+			// stampo gli id degli studenti promossi e bocciati
+			System.out.println("\n Promossi ids: ");
+			for(int i = 0; i<promossi.length; i++) {
+				System.out.println(promossi[i].id);
+			}
+			System.out.println("\n Bocciati ids: ");
+			for(int i = 0; i<bocciati.length; i++) {
+				System.out.println(bocciati[i].id);
+			}
+			
+			
+		
+		
+		
+		
+		
+		
 	}
-	
-	
 }
